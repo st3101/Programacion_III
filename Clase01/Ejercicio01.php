d52ca77069e44f1c7c3ea157f7513dbc563d734f
st3101 <santiago.t.leonardi@gmail.com>
2023-09-02 14:43:29 -0300

    Ejercicio 1 y 2

---
+++ Clase01/Ejercicio01.php
@@ -0,0 +1,12 @@
+<?php
+/* Aplicación No 1 (Mostrar variables)
+Realizar un programa que guarde su nombre en $nombre y su apellido en $apellido. Luego
+mostrar el contenido de las variables con el siguiente formato: Pérez, Juan. Utilizar el operador
+de concatenación. */
+
+$nombre = "Juan";
+$apellido = "Pérez";
+
+echo $nombre.", ".$apellido;
+
+?>
---
+++ Clase01/Ejercicio02.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Aplicación No 2 (Sumar dos números)
+Hacer un programa en PHP que sume el contenido de dos variables $x = -3 y $y = 15. Mostrar
+el resultado final. */
+
+$x = -3; 
+$y = 15;
+
+echo "Resultado: " . $x + $y;
