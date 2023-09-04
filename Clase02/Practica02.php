4b8b3b96a5b150762f964ffa8c1576ef13476144
st3101 <Santiago.t.leonardi@gmail.com>
2023-09-04 11:15:13 -0300

    Practica 2

---
+++ Clase02/Practica02.php
@@ -0,0 +1,6 @@
+<?php
+
+//require "./Practica01.php"; //Si no lo encuentra crea un "Fatal Error" y cierra la ejecucion
+
+include "./Practica01.php"; //Si no lo encuentra crea un "Warning" y sigue funcionando
+
