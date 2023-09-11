56c06b3f5644982b0474a7ede4f0241db0ea9745
st3101 <Santiago.t.leonardi@gmail.com>
2023-09-11 09:47:19 -0300

    Ejercicio 24

--- Clase02/Ejercicio24/Fabrica.php
+++ Clase02/Ejercicio24/Fabrica.php
@@ -1,18 +1,90 @@
 <?php 
 
-include "Operario.php"; 
+include_once "Operario.php"; 
 
 class Fabrica
 {
+    
     private int $_cantMaxOperarios;
-    private Operario $_operarios;
+    private $_operarios = array();
     private string $_razonSocial;
 
+    private array $a;
     public function __construct(string $rs)
     {
-        
+        $this->_razonSocial = $rs;
+        $this->_cantMaxOperarios = 5;        
     }
 
+    public function add(Operario $op):bool
+    {
+        $retorno = false;
+
+        if($op != null &&  $this->equals($this,$op) && $this->_cantMaxOperarios <= count($this->_operarios)) 
+        {
+            if(array_push($this->_operarios,$op))
+            {
+                $retorno = true;
+            } 
+        }
+
+        return $retorno;
+    }
+
+    public function equals(Fabrica $fb, Operario $op):bool
+    {
+        $retorno = false;
+        if($fb != null && $op != null)
+        {
+            foreach ($fb->_operarios as $value) 
+            {
+                if(Operario::equals($value,$op))
+                {
+                    $retorno =  true;
+                    break;
+                }
+            }
+        }
+        return $retorno;
+    }
+
+    public function mostrar():string
+    {
+        return "Razon Social: ".$this->_razonSocial ." - Cantidad Operarios: ".$this->_cantMaxOperarios."Operarios: ".$this->mostrarOperarios();
+    }
+
+    public static function mostrarCosto(Fabrica $fb):void
+    {
+        echo "Los costos son: ".$fb->retornarCostos();
+    }
+
+    private function mostrarOperarios():string
+    {
+        $text = "";
+        foreach ($this->_operarios as $value) 
+        {
+            $text .= $value->mostrar()."\n";
+        }
+
+        return $text;
+    }
+
+    public function remove(Operario $op)
+    {
+
+    }
+
+    private function retornarCostos():float
+    {
+        $costoTotal = 0;
+        foreach ($this->_operarios as $value) 
+        {
+            $costoTotal += $value->getSalario();
+        }
+        return $costoTotal;
+    }
 }
+
+
 
 
--- Clase02/Ejercicio24/Operario.php
+++ Clase02/Ejercicio24/Operario.php
@@ -7,14 +7,15 @@ class Operario
     private string $_nombre;
     private float $_salario;
 
-    public function  __construct(int $legajo, string $apellido, string $nombre)
+    public function  __construct(int $legajo, string $apellido, string $nombre, float $salario)
     {
         $this->_legajo = $legajo;
         $this->_apellido = $apellido;
         $this->_nombre = $nombre;
+        $this->_salario = $salario;
     }
 
-    public function equals(Operario $op1, Operario $op2):bool
+    public static function equals(Operario $op1, Operario $op2):bool
     {
         if($op1->_legajo == $op2->_legajo && $op1->_apellido == $op2->_apellido && $op1->_nombre == $op2->_nombre)
         {
@@ -43,14 +44,14 @@ class Operario
     {
         return $Op->mostrarStatic();
     }
-
     public function setAumentarSalario(float $aumento):void
     {
         $aumentoTotal = ($this->_salario * $aumento) / 100;
         $this->_salario = $this->_salario + $aumentoTotal;
     }
-        
 }
+
+
 
 
 
